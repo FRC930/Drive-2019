8ba71c0027306989a044f22541a982d51a3d7941
Nathankitzman <nkitzman11@gmail.com>
2019-02-12 08:45:10 -0600

    changed code to use elevator and added intake and arm solinoid

--- TestingDrive,EndGame,AndElevator/src/main/java/frc/robot/Robot.java
+++ TestingDrive,EndGame,AndElevator/src/main/java/frc/robot/Robot.java
@@ -31,7 +31,7 @@ import edu.wpi.first.wpilibj.Joystick;
  */
 public class Robot extends TimedRobot {
 
-    /*private static final CANSparkMax left1 = new CANSparkMax(1, MotorType.kBrushless);
+    private static final CANSparkMax left1 = new CANSparkMax(1, MotorType.kBrushless);
     private static final CANSparkMax left2 = new CANSparkMax(2, MotorType.kBrushless);
     private static final CANSparkMax left3 = new CANSparkMax(3, MotorType.kBrushless);
     private static final CANSparkMax right1 = new CANSparkMax(4, MotorType.kBrushless);
@@ -39,17 +39,25 @@ public class Robot extends TimedRobot {
     private static final CANSparkMax right3 = new CANSparkMax(6, MotorType.kBrushless);
     private static double stickX;
     private static double stickY;
-    /* end game
+    // end game
     private static final TalonSRX EndGameLift = new TalonSRX(1);
     private static final TalonSRX endgameLiftFollow1 = new TalonSRX(2);
     private static final TalonSRX endgameLiftFollow2 = new TalonSRX(3);
-    */
+    
     // evelvator
     public static TalonSRX lift1 = new TalonSRX(1);
     public static TalonSRX lift2 = new TalonSRX(2);
     public static double stickElev;
-
+    private static final Solenoid hatchPiston = new Solenoid(0);
     private static final Joystick stick = new Joystick(0);
+    private static final Joystick stick2 = new Joystick(1);
+    private static boolean Out = false;
+    private final static Solenoid armPiston = new Solenoid(3);
+    private static boolean Out2 = false;
+    private static boolean RtPressed = false;
+    private static boolean LtPressed = false;
+    private static boolean Rcheck = false;
+    private static boolean Lcheck = false;
     
    
   /**
@@ -59,7 +67,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     
-        /*left2.follow(left1);
+        left2.follow(left1);
         left3.follow(left1);
         
         right2.follow(right1);
@@ -67,11 +75,15 @@ public class Robot extends TimedRobot {
         
         endgameLiftFollow1.follow(EndGameLift);
         endgameLiftFollow2.follow(EndGameLift);
-        */
+        
         // elevator
         lift2.follow(lift1);
+        // hatchPiston.set(Out);
+        // armPiston.set(Out2);
         
-  }
+        }
+        
+  
 
   /**
    * This function is called every robot packet, no matter the mode. Use
@@ -113,18 +125,50 @@ public class Robot extends TimedRobot {
    * This function is called periodically during operator control.
    */
   @Override
-  public void teleopPeriodic() {
+  public void teleopPeriodic(){
+  // if(stick2.getRawAxis(3) >= 0.5 && RtPressed == false){
+  //   RtPressed = true;
+  // }
 
-    /*stickX = stick.getRawAxis(4);
+  // if(RtPressed == true && stick.getRawAxis(3) <= 0.5  ){
+  //   RtPressed = false;
+  //   Lcheck = false;
+  //   Rcheck = !Rcheck;
+  // }
+
+  // //Button LT test to see if pressed and released 
+  // if(stick2.getRawAxis(2) >= 0.5 && LtPressed == false){
+  //   LtPressed = true;
+  // }
+
+  // if(LtPressed == true && stick.getRawAxis(2) <= 0.5  ){
+  //   LtPressed = false;
+  //   Rcheck = false;
+  //   Lcheck = !Lcheck;
+  // }
+
+    if(stick2.getRawAxis(3) > 0.5)
+      hatchPiston.set(true);
+    if(stick2.getRawAxis(2) >0.5)
+      armPiston.set(true);   
+     if(stick2.getRawButton(1))
+      hatchPiston.set(false);
+    if(stick2.getRawButton(2))
+      armPiston.set(false); 
+    
+    
+    
+
+    stickX = stick.getRawAxis(4);
     stickY = stick.getRawAxis(1);
     stickX = -Math.pow(stickX,3);
-        stickY = Math.pow(stickY,3);
+    stickY = Math.pow(stickY,3);
 
     // Joystick deadband
-    if(Math.abs(stickX) < 0.000124){
+    if(Math.abs(stickX) < 0.005){
         stickX = 0;
     }
-    if(Math.abs(stickY) < 0.000124){
+    if(Math.abs(stickY) < 0.005){
         stickY = 0;
     }
 
@@ -133,18 +177,19 @@ public class Robot extends TimedRobot {
     runAt((stickY+stickX), -(stickY-stickX));
     
     // end game
-    if(stick.getRawButton(1) == true){
-      EndGameLift.set(ControlMode.PercentOutput, 1);
-    }
-    else if(stick.getRawButton(2) == true){
-       EndGameLift.set(ControlMode.PercentOutput, -1);
-    }
-    else{
-       EndGameLift.set(ControlMode.PercentOutput, 0);
-    }
-    */
+    // if(stick.getRawButton(1) == true){
+    //   EndGameLift.set(ControlMode.PercentOutput, 1);
+    // }
+    // else if(stick.getRawButton(2) == true){
+    //    EndGameLift.set(ControlMode.PercentOutput, -1);
+    // }
+    // else{
+    //    EndGameLift.set(ControlMode.PercentOutput, 0);
+    // }
+    
 
-    /*elevator
+    //elevator
+    /*
     if(stick.getRawButton(3) == true){
       lift1.set(ControlMode.PercentOutput, 1);
     }
@@ -158,16 +203,17 @@ public class Robot extends TimedRobot {
     }
     */
 
-    stickElev = Math.pow(stick.getRawAxis(1), 3);
+    stickElev = Math.pow(stick2.getRawAxis(1), 3);
 
-    if (Math.abs(stickElev) > 0.005) {
+    if (Math.abs(stickElev) > 0.0002) {
       lift1.set(ControlMode.PercentOutput, -stickElev);
     }
     else {
       lift1.set(ControlMode.PercentOutput, 0);
     }
-
   }
+
+  
       
   
 
@@ -179,8 +225,8 @@ public class Robot extends TimedRobot {
   }
   public static void runAt(double leftSpeed, double rightSpeed) {
         
-        //left1.set(leftSpeed);
-        //right1.set(rightSpeed);
+        left1.set(leftSpeed);
+        right1.set(rightSpeed);
 
     }
 }
